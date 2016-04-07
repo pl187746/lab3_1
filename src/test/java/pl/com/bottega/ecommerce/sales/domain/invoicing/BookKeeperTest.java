0042eb35376f478d555e644aee3f1686f6ea2662
Karol Kucharski <187746@edu.p.lodz.pl>
2016-04-07 17:45:01 +0200

    Refaktoryzacja testow.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,10 +1,12 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 
@@ -17,40 +19,47 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 public class BookKeeperTest {
 	
 	BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());;
 	
 	ClientData client = new ClientData(Id.generate(), "Jan Kowalski");
 	
+	Money someMoney = new Money(BigDecimal.valueOf(100));
+	
+	Tax someTax = new Tax(someMoney, "TT");
+	
 	ProductData productData;
+	
+	InvoiceRequest invoiceRequest;
+	
+	TaxPolicy taxPolicy;
 	
 	@Before
 	public void setUp() {
 		productData = mock(ProductData.class);
+		invoiceRequest = new InvoiceRequest(client);
+		taxPolicy = mock(TaxPolicy.class);
+		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(someTax);
+	}
+	
+	
+	private void addRequestItems(int n) {
+		for(int i = 0; i < n; ++i)
+			invoiceRequest.add(new RequestItem(productData, 1, someMoney));
 	}
 
 	@Test
 	public void zadanieFakturyZJednaPozycjaZwracaFaktureZJednaPozycja() {
-		InvoiceRequest invReq = new InvoiceRequest(client);
-		invReq.add(new RequestItem(productData, 1, new Money(BigDecimal.valueOf(100))));
-		TaxPolicy taxPolicy = mock(TaxPolicy.class);
-		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.valueOf(10)), "TT"));
-		Invoice invoice = bookKeeper.issuance(invReq, taxPolicy);
+		addRequestItems(1);
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 		assertThat(invoice.getItems().size(), equalTo(1));
 	}
 	
 	@Test
 	public void zadanieFakturyZDwomaPozycjamiWywolujeCalculateTaxDwaRazy() {
-		InvoiceRequest invReq = new InvoiceRequest(client);
-		invReq.add(new RequestItem(productData, 1, new Money(BigDecimal.valueOf(100))));
-		invReq.add(new RequestItem(productData, 1, new Money(BigDecimal.valueOf(200))));
-		TaxPolicy taxPolicy = mock(TaxPolicy.class);
-		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.valueOf(10)), "TT"));
-		bookKeeper.issuance(invReq, taxPolicy);
+		addRequestItems(2);
+		bookKeeper.issuance(invoiceRequest, taxPolicy);
 		verify(taxPolicy, times(2)).calculateTax(any(ProductType.class), any(Money.class));
 	}
 	
